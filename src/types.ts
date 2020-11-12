5498c1ef00f0eec0a63e0649908fc8a1b6f9a64c
CamilleAbella <camille.abella@hotmail.fr>
2020-11-12 14:34:43 +0100

    added types

---
+++ src/types.ts
@@ -0,0 +1,121 @@
+/**
+ * Containing date and time, following the ISO 8601 Extended format. <br>
+ * It looks like 2015-01-27T09:44:32.418Z. <br>
+ * All times are in UTC, and include milliseconds.
+ */
+export type RMDate = string;
+
+export type Role = "admin" | "dev" | "tiers";
+
+/**
+ * String containing a server-generated unique identifier.
+ */
+export type Id = string;
+
+/**
+ * String validated by email regex
+ */
+export type Email = string;
+
+export interface AccountMeta {
+  id: Id;
+  email: Email;
+  role: Role;
+  games?: Id[];
+}
+
+/**
+ * A game  Since RedMetrics data is open, <br>
+ * is is vital that no personally identifiable information is stored about a player.
+ * @property software - String that can be user agent on browsers.
+ * @property externalId - String that can be set by developers in order to link the player with another database. This must not be a personally identifiable marker such as an email address.
+ */
+export interface SessionMeta {
+  id: Id;
+  platform?: string;
+  screen_size?: string;
+  software?: string;
+  external_id?: string;
+  custom_data?: CustomData;
+}
+
+/**
+ * @property author - Containing the name of the person or organization who created the game
+ */
+export interface GameMeta {
+  id: Id;
+  name: string;
+  author?: string;
+  description?: string;
+  custom_data?: CustomData;
+}
+
+export interface GameVersionMeta {
+  name: string;
+  description?: string;
+  custom_data?: CustomData;
+}
+
+/**
+ * naming the type of progress event that occurred.
+ */
+export type EventType = StandardEventType | string;
+
+/**
+ * "start" created by default. <br>
+ * "end" close the session.
+ */
+export type StandardEventType =
+  | "start"
+  | "end"
+  | "win"
+  | "fail"
+  | "restart"
+  | "gain"
+  | "lose";
+
+/**
+ * Array of 2 or 3 integers describing where the event occurred in "game space".
+ */
+export type Coordinate = [number, number] | [number, number, number];
+
+/**
+ * what "level" the player was in when the event occured. <br>
+ * Adding more elements separated by points specifiy the section within a hierarchy. <br>
+ * For example, “level1.section2.subsection3”
+ */
+export type Section = string;
+
+/**
+ * JSON data associated with the session. <br>
+ * This must not contain personally identifiable markers such as name or exact address.
+ */
+export type CustomData = object;
+
+/**
+ * @property custom_data - JSON For "gain" and “lose” events, specifies the number of things are gained or lost.
+ */
+export interface RMEvent {
+  type: EventType;
+  server_time: RMDate;
+  user_time?: RMDate;
+  custom_data?: CustomData;
+  section?: Section;
+  coordinates?: Coordinate;
+}
+
+export interface RMError {
+  code: number;
+  description: string;
+}
+
+/**
+ * @property apiVersion - the numeric version, containing major and minor parts (such as 3.21)
+ * @property build - the build of the software
+ * @property startedAt - Date that the server was last started
+ */
+export interface Status {
+  api_version: number;
+  build: string;
+  started_at: RMDate;
+}
