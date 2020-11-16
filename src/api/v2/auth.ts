7f715bf851be614befe26204909ccc41d7a31273
CamilleAbella <camille.abella@hotmail.fr>
2020-11-16 16:28:43 +0100

    added auth and game endpoints

--- src/api/v2/auth.ts
+++ src/api/v2/auth.ts
@@ -1,9 +1,53 @@
-import expressAsyncHandler from "express-async-handler";
 import * as app from "../../app";
 
-app.v2.post(
-  "/login",
-  expressAsyncHandler(async (req, res) => {
-    // todo
-  })
-);
+app.v2.post("/login", (req, res) => {
+  // todo: Login to the system with valid username and password
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.post("/account", (req, res) => {
+  // todo:
+  //  Registers a new account.
+  //  An AccountMeta object should be sent in the body.
+  //  The Location response header will contain the URL for the new account.
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+/** “me” can be used instead of id to reference own account */
+app.v2.get("/account/:id", (req, res) => {
+  // todo:
+  //  Retrieves the AccountMeta for the given account.
+  //  Only admins can access accounts other than their own
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+/** “me” can be used instead of id to reference own account */
+app.v2.put("/account/:id", (req, res) => {
+  // todo:
+  //  Update the given account.
+  //  An AccountMeta object should be sent in the body.
+  //  Only admins can access accounts other than their own
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.post("/account/:id/reset-password", (req, res) => {
+  // todo: Request password reset. Requires confirmation by email
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.post("/account/:id/reset-password/confirm", (req, res) => {
+  // todo : Confirms password reset. Password reset token sent as URL param. Returns new password
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
---
+++ src/api/v2/game.ts
@@ -0,0 +1,50 @@
+import * as app from "../../app";
+
+app.v2.get(["/game", "/games"], (req, res) => {
+  // todo: Lists the games using the service as GameMeta objects (see section on Paging below)
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.post("/game", (req, res) => {
+  // todo:
+  //  Creates a new game.
+  //  A GameMeta object should be sent in the body.
+  //  A default version of the game will be created.
+  //  The Location response header will contain the URL for the new game.
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.get("/game/:id", (req, res) => {
+  // todo: Retrieves information about the game with that Id as a GameMeta object
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.put("/game/:id", (req, res) => {
+  // todo: Updates game information with the provided GameMeta.
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.get("/game/:id/version", (req, res) => {
+  // todo: Lists versions of the the game with that Id as GameVersionMeta objects (see section on Paging below)
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
+
+app.v2.post("/game/:id/version", (req, res) => {
+  // todo:
+  //  Creates a new version of the game.
+  //  A GameVersionMeta object should be sent in the body.
+  //  The Location response header will contain the URL for the new game.
+  res.status(404).json({
+    error: "not implemented, ",
+  });
+});
