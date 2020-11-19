016f51fd20bfeab2aad553b5082c9e1f15d7eb2f
CamilleAbella <camille.abella@hotmail.fr>
2020-11-19 14:53:39 +0100

    refactored auth and events routes

--- src/api/v2/auth.ts
+++ src/api/v2/auth.ts
@@ -18,25 +18,25 @@ app.v2.post("/account", (req, res) => {
 });
 
 /** “me” can be used instead of id to reference own account */
-app.v2.get("/account/:id", (req, res) => {
-  // todo:
-  //  Retrieves the AccountMeta for the given account.
-  //  Only admins can access accounts other than their own
-  res.status(404).json({
-    error: "not implemented, ",
+app.v2
+  .route("/account/:id")
+  .get((req, res) => {
+    // todo:
+    //  Retrieves the AccountMeta for the given account.
+    //  Only admins can access accounts other than their own
+    res.status(404).json({
+      error: "not implemented, ",
+    });
+  })
+  .put((req, res) => {
+    // todo:
+    //  Update the given account.
+    //  An AccountMeta object should be sent in the body.
+    //  Only admins can access accounts other than their own
+    res.status(404).json({
+      error: "not implemented, ",
+    });
   });
-});
-
-/** “me” can be used instead of id to reference own account */
-app.v2.put("/account/:id", (req, res) => {
-  // todo:
-  //  Update the given account.
-  //  An AccountMeta object should be sent in the body.
-  //  Only admins can access accounts other than their own
-  res.status(404).json({
-    error: "not implemented, ",
-  });
-});
 
 app.v2.post("/account/:id/reset-password", (req, res) => {
   // todo: Request password reset. Requires confirmation by email
