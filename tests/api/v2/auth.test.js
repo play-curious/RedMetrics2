8a31d5c9f6e7304f1436f5d4fcd47710b253c7b6
CamilleAbella <camille.abella@hotmail.fr>
2020-12-01 15:34:11 +0100

    tested POST login route

--- tests/api/v2/auth.test.js
+++ tests/api/v2/auth.test.js
@@ -3,10 +3,12 @@ const app = require("../../../dist/app");
 
 describe("auth", () => {
   describe("register", () => {
+    const route = "/api/v2/rest/account";
+
     describe("fails", () => {
       test("missing email", (done) => {
         request(app.server)
-          .post("/api/v2/rest/account")
+          .post(route)
           .send({
             password: "test",
             type: "user",
@@ -17,7 +19,7 @@ describe("auth", () => {
 
       test("missing password", (done) => {
         request(app.server)
-          .post("/api/v2/rest/account")
+          .post(route)
           .send({
             email: "test@test.test",
             type: "user",
@@ -28,7 +30,7 @@ describe("auth", () => {
 
       test("invalid email", (done) => {
         request(app.server)
-          .post("/api/v2/rest/account")
+          .post(route)
           .send({
             email: "test",
             password: "test",
@@ -42,7 +44,7 @@ describe("auth", () => {
     describe("success", () => {
       test("register as user", (done) => {
         request(app.server)
-          .post("/api/v2/rest/account")
+          .post(route)
           .send({
             email: "user@test.test",
             password: "test",
@@ -54,7 +56,7 @@ describe("auth", () => {
 
       test("register as dev", (done) => {
         request(app.server)
-          .post("/api/v2/rest/account")
+          .post(route)
           .send({
             email: "dev@test.test",
             password: "test",
@@ -66,7 +68,7 @@ describe("auth", () => {
 
       test("register as already registered user", (done) => {
         request(app.server)
-          .post("/api/v2/rest/account")
+          .post(route)
           .send({
             email: "user@test.test",
             password: "test",
@@ -78,5 +80,75 @@ describe("auth", () => {
     });
   });
 
-  test("login", (done) => {});
+  describe("login", () => {
+    const route = "/api/v2/rest/login";
+
+    describe("fails", () => {
+      test("missing email", (done) => {
+        request(app.server)
+          .post(route)
+          .send({
+            password: "test",
+          })
+          .expect(401)
+          .end(done);
+      });
+
+      test("missing password", (done) => {
+        request(app.server)
+          .post(route)
+          .send({
+            email: "user@test.test",
+          })
+          .expect(401)
+          .end(done);
+      });
+
+      test("invalid email", (done) => {
+        request(app.server)
+          .post(route)
+          .send({
+            email: "test",
+            password: "test",
+          })
+          .expect(401)
+          .end(done);
+      });
+
+      test("unknown email", (done) => {
+        request(app.server)
+          .post(route)
+          .send({
+            email: "unknown@test.test",
+            password: "test",
+          })
+          .expect(300)
+          .end(done);
+      });
+
+      test("incorrect password", (done) => {
+        request(app.server)
+          .post(route)
+          .send({
+            email: "user@test.test",
+            password: "incorrect",
+          })
+          .expect(401)
+          .end(done);
+      });
+    });
+
+    describe("success", () => {
+      test("login", (done) => {
+        request(app.server)
+          .post(route)
+          .send({
+            email: "user@test.test",
+            password: "test",
+          })
+          .expect(200)
+          .end(done);
+      });
+    });
+  });
 });
