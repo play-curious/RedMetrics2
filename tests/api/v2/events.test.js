df293a365ff3c53fb94f9d93dc67bb04996c1200
CamilleAbella <camille.abella@hotmail.fr>
2020-12-03 15:33:29 +0100

    tested GET /session/:id route

--- tests/api/v2/events.test.js
+++ tests/api/v2/events.test.js
@@ -4,6 +4,10 @@ const app = require("../../../dist/app");
 const { user_tokens, user_ids } = require("./auth.test");
 const { game_version_ids } = require("./game.test");
 
+const session_ids = new Map();
+
+module.exports = { session_ids };
+
 describe("events", () => {
   describe("session", () => {
     describe("post", () => {
@@ -46,38 +50,57 @@ describe("events", () => {
               },
             })
             .expect(200)
-            .end(done);
+            .end((err, res) => {
+              session_ids.set("session", res.body.id);
+              done(err);
+            });
         });
       });
     });
 
     {
-      const route = (id) => `/session/${id}`
+      const route = (id) => `/session/${id}`;
 
       describe("get", () => {
         describe("fails", () => {
           test("missing token", (done) => {
+            test("missing token", (done) => {
+              request(app.server)
+                .get(route(session_ids.get("session")))
+                .expect(401)
+                .end(done);
+            });
 
-          })
-        })
+            // todo: missing permission ? (only dev of game or other cases)
+
+            test("missing session", (done) => {
+              request(app.server)
+                .get(route(-1))
+                .set("Authorization", `bearer ${user_tokens.get("admin")}`)
+                .expect(404)
+                .end(done);
+            });
+          });
+        });
 
         describe("success", () => {
-
-        })
-      })
+          test("get session", (done) => {
+            request(app.server)
+              .get(route(session_ids.get("session")))
+              .set("Authorization", `bearer ${user_tokens.get("admin")}`)
+              .expect(404)
+              .end(done);
+          });
+        });
+      });
 
       describe("update", () => {
         describe("fails", () => {
-          test("missing token", (done) => {
+          test("missing token", (done) => {});
+        });
 
-          })
-        })
-
-        describe("success", () => {
-
-        })
-      })
+        describe("success", () => {});
+      });
     }
-
   });
 });
