b55d1a26c36436dc339307786ecbdab617c81c44
CamilleAbella <camille.abella@hotmail.fr>
2020-12-15 17:03:11 +0100

    fixed EJS templates

--- src/api/status.ts
+++ src/api/status.ts
@@ -6,12 +6,20 @@ const project = require("../../package.json");
 app.server.get(
   "/status",
   expressAsyncHandler(async (req, res) => {
-    res.json({
+    const json = req.query.type === "json";
+
+    const output = {
       deployedAt: new Date(app.server.locals.site.deployedAt),
       uptime: Date.now() - app.server.locals.site.deployedAt,
       uptimeText: app.server.locals.site.deployedSince(),
       version: project.version,
       license: project.license,
-    });
+    };
+
+    if (json) {
+      res.json(output);
+    } else {
+      res.render("pages/status", output);
+    }
   })
 );
