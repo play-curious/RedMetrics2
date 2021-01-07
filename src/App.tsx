3fdb8baca65a926ae8dc4f352155ae6ea89cff60
CamilleAbella <camille.abella@hotmail.fr>
2021-01-07 14:04:46 +0100

    start to create front-end app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Register from "./views/Register";
+import Home from "./views/Home";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state: {
+    role: "nobody" | "admin" | "dev" | "user"
+  } = {
+    role: "nobody"
+  }
+
+  render() {
+    if(this.state.role === "nobody"){
+      const apiKey = sessionStorage.getItem("apiKey")
+
+      if(!apiKey) {
+        return <Register/>;
+      }else{
+        // setState
+      }
+    }else{
+      return <Home/>;
+    }
+  }
 }
 
 export default App;
