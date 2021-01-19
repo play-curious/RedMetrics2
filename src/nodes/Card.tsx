2ff1b4c003e94e68e7baca901e95ebaa038aff3c
CamilleAbella <camille.abella@hotmail.fr>
2021-01-19 14:45:16 +0100

    convert Card components to hook based function

--- src/nodes/Card.tsx
+++ src/nodes/Card.tsx
@@ -1,33 +1,24 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
 import "./Card.scss";
 
-export default abstract class Card extends React.Component {
-  props = {
-    title: "Mon titre",
-    description: "Ma description",
-    fields: [],
-    url: "",
-  };
+const Card: FunctionComponent<{
+  title: string;
+  description: string;
+  fields: string[];
+  url: string;
+}> = ({ title, description, fields, url }) => (
+  <>
+    <Link className="card" to={url}>
+      <h3> {title} </h3>
+      <p> {description} </p>
+      <div>
+        {fields.map((field) => {
+          return <p> {field} </p>;
+        })}
+      </div>
+    </Link>
+  </>
+);
 
-  state = {
-    selected: false,
-  };
-
-  render() {
-    return (
-      <Link
-        className={`card ${this.state.selected && "selected"}`}
-        to={this.props.url}
-      >
-        <h3> {this.props.title} </h3>
-        <p> {this.props.description} </p>
-        <div>
-          {this.props.fields.map((field) => {
-            return <p> {field} </p>;
-          })}
-        </div>
-      </Link>
-    );
-  }
-}
+export default Card;
