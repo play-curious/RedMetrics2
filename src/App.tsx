967d7f66aa1b859e9d81fd49cab0e21dde1875a0
CamilleAbella <camille.abella@hotmail.fr>
2021-01-19 11:54:41 +0100

    convert Login and App components to hook based functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import Register from "./views/auth/Register";
 import Login from "./views/auth/Login";
 import Home from "./views/user/Home";
@@ -12,60 +12,42 @@ import Profile from "./views/user/Profile";
 
 import "./App.scss";
 
-class App extends React.Component {
-  state: {
-    apiKey: string | null;
-  } = {
-    apiKey: null,
-  };
+export default function App() {
+  const [apiKey, setApiKey] = useState<string | null>(
+    sessionStorage.getItem("apiKey")
+  );
 
-  componentDidMount() {
-    const apiKey = sessionStorage.getItem("apiKey");
-
-    if (apiKey) {
-      this.setState({ apiKey });
-    }
-  }
-
-  handleApiKeyChange = (apiKey: string) => {
-    this.setState({ apiKey });
-  };
-
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home apiKey={this.state.apiKey} />
-          </Route>
-          <Route exact path="/register">
-            <Register onApiKeyChange={this.handleApiKeyChange} />
-          </Route>
-          <Route exact path="/login">
-            <Login onApiKeyChange={this.handleApiKeyChange} />
-          </Route>
-          <Route exact path="/error">
-            <AppError code={0} message={""} />
-          </Route>
-          <Route exact path="/accounts">
-            <Accounts />
-          </Route>
-          <Route exact path="/docs">
-            <Documentation />
-          </Route>
-          <Route exact path="/tutorial">
-            <GettingStarted />
-          </Route>
-          <Route exact path="/search">
-            <Search />
-          </Route>
-          <Route exact path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home apiKey={apiKey} />
+        </Route>
+        <Route exact path="/register">
+          <Register onApiKeyChange={setApiKey} />
+        </Route>
+        <Route exact path="/login">
+          <Login onApiKeyChange={setApiKey} />
+        </Route>
+        <Route exact path="/error">
+          <AppError code={0} message={""} />
+        </Route>
+        <Route exact path="/accounts">
+          <Accounts />
+        </Route>
+        <Route exact path="/docs">
+          <Documentation />
+        </Route>
+        <Route exact path="/tutorial">
+          <GettingStarted />
+        </Route>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -1,85 +1,67 @@
 import axios from "axios";
-import React from "react";
+import React, { FunctionComponent, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Redirect } from "react-router";
 import { Link } from "react-router-dom";
 import * as constants from "../../constants";
 
-export default class Login extends React.Component {
-  props: {
-    onApiKeyChange: ((apiKey: string) => void) | null;
-  } = {
-    onApiKeyChange: null,
-  };
-
-  state: {
-    email: string;
-    password: string;
-    redirect: null | string;
-  } = {
-    email: "",
-    password: "",
-    redirect: null,
-  };
-
-  handleChange = (event: any) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-
-    try {
-      const response = await axios.post(
-        "login",
-        {
-          email: this.state.email,
-          password: this.state.password,
-        },
-        {
-          baseURL: constants.apiBaseURL,
-        }
-      );
-
-      this.props.onApiKeyChange?.(response.data.apiKey);
-
-      sessionStorage.setItem("apiKey", response.data.apiKey);
-
-      this.setState({ redirect: "/home", password: "" });
-    } catch (error) {
-      this.setState({ redirect: "/error", password: "" });
-    }
-  };
-
-  render() {
-    return (
-      <div className="Login">
-        {this.state.redirect && <Redirect to={this.state.redirect} />}
-        <div className="center">
-          <h1> Login </h1>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              placeholder="Email"
-              onChange={this.handleChange}
-            />
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              placeholder="Password"
-              onChange={this.handleChange}
-            />
-            <input className="button" type="submit" value="Go" />
-          </form>
-        </div>
-        <Link className="button" to={{ pathname: "/login" }}>
-          Register
-        </Link>
-      </div>
-    );
-  }
+interface LoginForm {
+  email: string;
+  password: string;
 }
+
+const Login: FunctionComponent<{
+  onApiKeyChange: (apiKey: string) => void;
+}> = ({ onApiKeyChange }) => {
+  const [redirect, setRedirect] = useState<null | string>(null);
+
+  const { register, handleSubmit, setValue } = useForm<LoginForm>();
+
+  const afterSubmit = (data: LoginForm) => {
+    axios
+      .post("login", data, {
+        baseURL: constants.apiBaseURL,
+      })
+      .then((response) => {
+        onApiKeyChange(response.data.apiKey);
+        sessionStorage.setItem("apiKey", response.data.apiKey);
+        setRedirect("/home");
+      })
+      .catch((error) => {
+        setRedirect("/error");
+      })
+      .finally(() => setValue("password", ""));
+  };
+
+  return (
+    <>
+      {redirect && <Redirect to={redirect} />}
+      <div className="center">
+        <h1> Login </h1>
+        <form onSubmit={handleSubmit(afterSubmit)}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            ref={register({
+              pattern: /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i,
+              required: true,
+            })}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            ref={register({})}
+          />
+          <input className="button" type="submit" value="Go" />
+        </form>
+      </div>
+      <Link className="button" to={{ pathname: "/login" }}>
+        Register
+      </Link>
+    </>
+  );
+};
+
+export default Login;
