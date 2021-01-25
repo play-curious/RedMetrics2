bdb52eb9aacbb5603386f79c83a6f2dac87d2984
CamilleAbella <camille.abella@hotmail.fr>
2021-01-25 14:14:39 +0100

    add card styles

--- src/nodes/Card.tsx
+++ src/nodes/Card.tsx
@@ -7,17 +7,20 @@ const Card: React.FunctionComponent<{
   fields: string[];
   url: string;
 }> = ({ title, description, fields, url }) => (
-  <>
-    <Dom.Link className="card" to={url}>
-      <h3> {title} </h3>
-      <p> {description} </p>
-      <div>
+  <Dom.Link className="max-w-2xl mx-auto sm:px-6 lg:px-8" to={url}>
+    <div className="overflow-hidden shadow-md">
+      <div className="px-6 py-4 bg-white border-b border-gray-200 font-bold uppercase">
+        {title}
+      </div>
+      <div className="p-6 bg-white border-b border-gray-200">
+        {description}{" "}
         {fields.map((field) => {
           return <p> {field} </p>;
         })}
       </div>
-    </Dom.Link>
-  </>
+      <div className="p-6 bg-white border-gray-200 text-right">footer</div>
+    </div>
+  </Dom.Link>
 );
 
 export default Card;
