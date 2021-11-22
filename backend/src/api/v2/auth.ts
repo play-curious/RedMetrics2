e559fb2aa89debfbf13470652d9efeff6da21c9c
CamilleAbella <camille.abella@hotmail.fr>
2021-11-22 16:29:19 +0100

    uses route maker in api.v2.auth.ts file

--- backend/src/api/v2/auth.ts
+++ backend/src/api/v2/auth.ts
@@ -9,7 +9,10 @@ import * as auth from "../../controllers/auth";
 import * as game from "../../controllers/game";
 import * as types from "rm2-typings";
 
-app.v2.get(
+const route = types.utils.buildRouteMaker(app.v2);
+
+route<types.api.Logout>(
+  "Get",
   "/logout",
   utils.authentication(),
   expressAsyncHandler(async (req, res) => {
@@ -18,7 +21,8 @@ app.v2.get(
   })
 );
 
-app.v2.post(
+route<types.api.Login>(
+  "Post",
   "/login",
   expressAsyncHandler(async (req, res) => {
     // Login to the system with valid username and password
@@ -67,7 +71,8 @@ app.v2.post(
   })
 );
 
-app.v2.post(
+route<types.api.Register>(
+  "Post",
   "/register",
   expressAsyncHandler(async (req, res) => {
     //  Registers a new account.
@@ -124,177 +129,187 @@ app.v2.post(
   })
 );
 
-app.v2
-  .route("/account")
-  .get(utils.authentication(), (req, res) => {
+route<types.api.Account>(
+  "Get",
+  "/account",
+  utils.authentication(),
+  (req, res) => {
     if (utils.hasAccount(req)) res.json(req.account);
+  }
+);
+
+route<types.api.Account>(
+  "Post",
+  "/account",
+  utils.authentication("admin", true),
+  expressAsyncHandler(async (req, res) => {
+    //  Registers a new account.
+    //  An AccountMeta object should be sent in the body.
+    //  The Location response header will contain the URL for the new account.
+
+    const email = req.body?.email,
+      password = req.body?.password,
+      is_admin = /^(?:true|1|)$/.test(String(req.body?.is_admin ?? "false"));
+
+    if (!email || !password)
+      return utils.sendError(res, {
+        code: 401,
+        description: "Missing email or password",
+      });
+
+    if (!utils.isValidEmail(email))
+      return utils.sendError(res, {
+        code: 401,
+        description: "Invalid email",
+      });
+
+    if (await auth.emailAlreadyUsed(email))
+      return utils.sendError(res, {
+        code: 401,
+        description: "Already used email",
+      });
+
+    const hash = await bcrypt.hash(
+      password,
+      parseInt(process.env.SALT_ROUNDS as string)
+    );
+
+    const id = await auth.postAccount({
+      email,
+      password: hash,
+      is_admin,
+      confirmed: false,
+      created_timestamp: String(Date.now()),
+    });
+
+    const account = await auth.getAccount(id);
+
+    if (!account) return;
+
+    await utils.sendAccountConfirmation(account);
+
+    res.sendStatus(200);
   })
-  .post(
-    utils.authentication("admin", true),
-    expressAsyncHandler(async (req, res) => {
-      //  Registers a new account.
-      //  An AccountMeta object should be sent in the body.
-      //  The Location response header will contain the URL for the new account.
+);
 
-      const email = req.body?.email,
-        password = req.body?.password,
-        is_admin = /^(?:true|1|)$/.test(String(req.body?.is_admin ?? "false"));
+route<types.api.AccountById>(
+  "Delete",
+  "/account/:id",
+  utils.authentication(
+    (context) => context.params.id === context.account.id,
+    true
+  ),
+  expressAsyncHandler(async (req, res) => {
+    await auth.deleteAccount(req.params.id);
+    res.sendStatus(200);
+  })
+);
 
-      if (!email || !password)
+route<types.api.AccountById>(
+  "Get",
+  "/account/:id",
+  utils.authentication((context) => context.params.id === context.account.id),
+  expressAsyncHandler(async (req, res) => {
+    //  Retrieves the AccountMeta for the given account.
+    //  Only admins can access accounts other than their own
+
+    const account = await auth.getAccount(req.params.id);
+
+    if (!account)
+      return utils.sendError(res, {
+        code: 404,
+        description: "Account not found",
+      });
+
+    res.json(account);
+  })
+);
+
+route<types.api.AccountById>(
+  "Put",
+  "/account/:id",
+  utils.authentication(
+    async (context) =>
+      context.params.id === context.account.id || context.account.is_admin,
+    true
+  ),
+  expressAsyncHandler(async (req, res) => {
+    //  Update the given account.
+    //  An AccountMeta object should be sent in the body.
+    //  Only admins can access accounts other than their own
+
+    if (!utils.hasAccount(req)) return;
+
+    const body = req.body as types.api.AccountById["Methods"]["Put"]["Body"];
+
+    const id = req.params.id;
+
+    const is_admin = req.account.is_admin ? req.body?.is_admin : undefined;
+
+    if (body.email && !utils.isValidEmail(body.email)) {
+      return utils.sendError(res, {
+        code: 401,
+        description: "Invalid email",
+      });
+    }
+
+    const account = await auth.getAccount(id);
+
+    if (!account)
+      return utils.sendError(res, {
+        code: 404,
+        description: "Account not found",
+      });
+
+    if ((account.id === id && !req.account.is_admin) || !req.account.is_admin) {
+      if (!body.old_password)
         return utils.sendError(res, {
           code: 401,
-          description: "Missing email or password",
+          description: "Missing old_password",
         });
 
-      if (!utils.isValidEmail(email))
+      if (!(await bcrypt.compare(body.old_password, account.password)))
         return utils.sendError(res, {
           code: 401,
-          description: "Invalid email",
+          description: "Incorrect password",
         });
+    }
 
-      if (await auth.emailAlreadyUsed(email))
-        return utils.sendError(res, {
-          code: 401,
-          description: "Already used email",
-        });
+    let hash: string | undefined;
 
-      const hash = await bcrypt.hash(
-        password,
+    if (body.new_password)
+      hash = await bcrypt.hash(
+        body.new_password,
         parseInt(process.env.SALT_ROUNDS as string)
       );
 
-      const id = await auth.postAccount({
-        email,
-        password: hash,
-        is_admin,
-        confirmed: false,
-        created_timestamp: String(Date.now()),
-      });
+    await auth.updateAccount(id, {
+      email: body.email,
+      password: hash,
+      is_admin,
+      confirmed: account.confirmed
+        ? account.email === body.email
+          ? account.confirmed
+          : false
+        : false,
+    });
 
-      const account = await auth.getAccount(id);
+    res.json({ id });
+  })
+);
 
-      if (!account) return;
+route<types.api.AccountById_Keys>(
+  "Get",
+  "/account/:id/keys",
+  utils.authentication((ctx) => ctx.params.id === ctx.account.id),
+  expressAsyncHandler(async (req, res) => {
+    if (utils.hasAccount(req))
+      res.json(await auth.getUserApiKeys(req.account.id));
+  })
+);
 
-      await utils.sendAccountConfirmation(account);
-
-      res.sendStatus(200);
-    })
-  );
-
-app.v2
-  .route("/account/:id")
-  .delete(
-    utils.authentication(
-      (context) => context.params.id === context.account.id,
-      true
-    ),
-    expressAsyncHandler(async (req, res) => {
-      await auth.deleteAccount(req.params.id);
-      res.sendStatus(200);
-    })
-  )
-  .get(
-    utils.authentication((context) => context.params.id === context.account.id),
-    expressAsyncHandler(async (req, res) => {
-      //  Retrieves the AccountMeta for the given account.
-      //  Only admins can access accounts other than their own
-
-      const account = await auth.getAccount(req.params.id);
-
-      if (!account)
-        return utils.sendError(res, {
-          code: 404,
-          description: "Account not found",
-        });
-
-      res.json(account);
-    })
-  )
-  .put(
-    utils.authentication(
-      async (context) =>
-        context.params.id === context.account.id || context.account.is_admin,
-      true
-    ),
-    expressAsyncHandler(async (req, res) => {
-      //  Update the given account.
-      //  An AccountMeta object should be sent in the body.
-      //  Only admins can access accounts other than their own
-
-      if (!utils.hasAccount(req)) return;
-
-      const body = req.body as types.api.AccountById["Put"]["Body"];
-
-      const id = req.params.id;
-
-      const is_admin = req.account.is_admin ? req.body?.is_admin : undefined;
-
-      if (body.email && !utils.isValidEmail(body.email)) {
-        return utils.sendError(res, {
-          code: 401,
-          description: "Invalid email",
-        });
-      }
-
-      const account = await auth.getAccount(id);
-
-      if (!account)
-        return utils.sendError(res, {
-          code: 404,
-          description: "Account not found",
-        });
-
-      if (
-        (account.id === id && !req.account.is_admin) ||
-        !req.account.is_admin
-      ) {
-        if (!body.old_password)
-          return utils.sendError(res, {
-            code: 401,
-            description: "Missing old_password",
-          });
-
-        if (!(await bcrypt.compare(body.old_password, account.password)))
-          return utils.sendError(res, {
-            code: 401,
-            description: "Incorrect password",
-          });
-      }
-
-      let hash: string | undefined;
-
-      if (body.new_password)
-        hash = await bcrypt.hash(
-          body.new_password,
-          parseInt(process.env.SALT_ROUNDS as string)
-        );
-
-      await auth.updateAccount(id, {
-        email: body.email,
-        password: hash,
-        is_admin,
-        confirmed: account.confirmed
-          ? account.email === body.email
-            ? account.confirmed
-            : false
-          : false,
-      });
-
-      res.json({ id });
-    })
-  );
-
-app.v2
-  .route("/account/:id/keys")
-  .all(utils.authentication((ctx) => ctx.params.id === ctx.account.id))
-  .get(
-    expressAsyncHandler(async (req, res) => {
-      if (utils.hasAccount(req))
-        res.json(await auth.getUserApiKeys(req.account.id));
-    })
-  );
-
-app.v2.get(
+route<types.api.Accounts>(
+  "Get",
   "/accounts",
   utils.authentication("admin", true),
   expressAsyncHandler(async (req, res) => {
@@ -302,68 +317,77 @@ app.v2.get(
   })
 );
 
-app.v2
-  .route("/key")
-  .all(utils.authentication(undefined, true))
-  .post(
-    expressAsyncHandler(async (req, res) => {
-      if (!utils.hasAccount(req)) return;
+route<types.api.Key>(
+  "Post",
+  "/key",
+  utils.authentication(undefined, true),
+  expressAsyncHandler(async (req, res) => {
+    if (!utils.hasAccount(req)) return;
 
-      if (!req.body.game_id)
-        return utils.sendError(res, {
-          code: 400,
-          description: "Missing 'game_id' property in body",
-        });
+    if (!req.body.game_id)
+      return utils.sendError(res, {
+        code: 400,
+        description: "Missing 'game_id' property in body",
+      });
 
-      const currentGame = await game.getGame(req.body.game_id);
+    const currentGame = await game.getGame(req.body.game_id);
 
-      if (!currentGame)
-        return utils.sendError(res, {
-          code: 404,
-          description: "Game not found",
-        });
+    if (!currentGame)
+      return utils.sendError(res, {
+        code: 404,
+        description: "Game not found",
+      });
 
-      const apiKey: types.tables.ApiKey = {
-        description: req.body.description,
-        start_at: new Date().toISOString(),
-        account_id: req.account.id,
-        key: uuid.v4(),
-        game_id: req.body.game_id,
-      };
+    const apiKey: types.tables.ApiKey = {
+      description: req.body.description,
+      start_at: new Date().toISOString(),
+      account_id: req.account.id,
+      key: uuid.v4(),
+      game_id: req.body.game_id,
+    };
 
-      await auth.apiKeys().insert(apiKey);
+    await auth.apiKeys().insert(apiKey);
 
-      res.json(apiKey);
-    })
-  )
-  .get(
-    expressAsyncHandler(async (req, res) => {
-      if (!utils.hasGame(req))
-        return utils.sendError(res, {
-          code: 400,
-          description: "You need to access this route via API key.",
-        });
+    res.json(apiKey);
+  })
+);
 
-      const apiKey = await auth.getApiKey(req.apiKey);
+route<types.api.Key>(
+  "Get",
+  "/key",
+  utils.authentication(undefined, true),
+  expressAsyncHandler(async (req, res) => {
+    if (!utils.hasGame(req))
+      return utils.sendError(res, {
+        code: 400,
+        description: "You need to access this route via API key.",
+      });
 
-      if (!apiKey)
-        return utils.sendError(res, {
-          code: 404,
-          description: "API key not found",
-        });
+    const apiKey = await auth.getApiKey(req.apiKey);
 
-      return res.json(apiKey);
-    })
-  )
-  .delete(
-    expressAsyncHandler(async (req, res) => {
-      if (!utils.hasAccount(req)) return;
-      await auth.removeUserApiKeys(req.account.id);
-      res.sendStatus(200);
-    })
-  );
+    if (!apiKey)
+      return utils.sendError(res, {
+        code: 404,
+        description: "API key not found",
+      });
 
-app.v2.delete(
+    return res.json(apiKey);
+  })
+);
+
+route<types.api.Key>(
+  "Delete",
+  "/key",
+  utils.authentication(undefined, true),
+  expressAsyncHandler(async (req, res) => {
+    if (!utils.hasAccount(req)) return;
+    await auth.removeUserApiKeys(req.account.id);
+    res.sendStatus(200);
+  })
+);
+
+route<types.api.KeyByKey>(
+  "Delete",
   "/key/:key",
   utils.authentication(async (context) => {
     const apiKey = await auth.getApiKey(context.params.key);
@@ -376,92 +400,92 @@ app.v2.delete(
   })
 );
 
-app.v2
-  .route("/lost-password")
-  .post(
-    expressAsyncHandler(async (req, res) => {
-      const email: types.api.LostPassword["Post"]["Body"]["email"] =
-        req.body.email;
+route<types.api.LostPassword>(
+  "Post",
+  "/lost-password",
+  expressAsyncHandler(async (req, res) => {
+    const email: types.api.LostPassword["Methods"]["Post"]["Body"]["email"] =
+      req.body.email;
 
-      if (!email)
-        return utils.sendError(res, {
-          code: 401,
-          description: "Missing email",
-        });
+    if (!email)
+      return utils.sendError(res, {
+        code: 401,
+        description: "Missing email",
+      });
 
-      const account = await auth.getAccountByEmail(email);
+    const account = await auth.getAccountByEmail(email);
 
-      if (!account)
-        return utils.sendError(res, {
-          code: 404,
-          description: "Account not found",
-        });
+    if (!account)
+      return utils.sendError(res, {
+        code: 404,
+        description: "Account not found",
+      });
 
-      // send code by email !
-      await utils.sendDigitCode(
-        account,
-        `<h1> Reset your password - Step 1/2 </h1>
+    // send code by email !
+    await utils.sendDigitCode(
+      account,
+      `<h1> Reset your password - Step 1/2 </h1>
         <h2> To reset your password, use the following code. </h2>`,
-        "RedMetrics2 - Reset your password"
-      );
+      "RedMetrics2 - Reset your password"
+    );
 
-      res.sendStatus(200);
-    })
-  )
-  .patch(
-    expressAsyncHandler(async (req, res) => {
-      const code: types.api.LostPassword["Patch"]["Body"]["code"] =
-        req.body.code;
+    res.sendStatus(200);
+  })
+);
 
-      const confirmation = await auth
-        .confirmations()
-        .where("code", code)
-        .first();
+route<types.api.LostPassword>(
+  "Patch",
+  "/lost-password",
+  expressAsyncHandler(async (req, res) => {
+    const code: types.api.LostPassword["Methods"]["Patch"]["Body"]["code"] =
+      req.body.code;
 
-      if (!confirmation)
-        return utils.sendError(res, {
-          code: 422,
-          description: "Confirmation code not valid",
-        });
+    const confirmation = await auth.confirmations().where("code", code).first();
 
-      await auth
-        .confirmations()
-        .where("account_id", confirmation.account_id)
-        .delete();
+    if (!confirmation)
+      return utils.sendError(res, {
+        code: 422,
+        description: "Confirmation code not valid",
+      });
 
-      const account = await auth.getAccount(confirmation.account_id);
+    await auth
+      .confirmations()
+      .where("account_id", confirmation.account_id)
+      .delete();
 
-      if (!account) {
-        return utils.sendError(res, {
-          code: 404,
-          description: "Account not found",
-        });
-      }
+    const account = await auth.getAccount(confirmation.account_id);
 
-      const newPassword = uuid.v4();
+    if (!account) {
+      return utils.sendError(res, {
+        code: 404,
+        description: "Account not found",
+      });
+    }
 
-      const hash = await bcrypt.hash(
-        newPassword,
-        parseInt(process.env.SALT_ROUNDS as string)
-      );
+    const newPassword = uuid.v4();
 
-      await auth.updateAccount(account.id, { password: hash });
+    const hash = await bcrypt.hash(
+      newPassword,
+      parseInt(process.env.SALT_ROUNDS as string)
+    );
 
-      // send password by email !
-      await utils.sendMail({
-        to: account.email,
-        subject: "Reset your password",
-        text: `
+    await auth.updateAccount(account.id, { password: hash });
+
+    // send password by email !
+    await utils.sendMail({
+      to: account.email,
+      subject: "Reset your password",
+      text: `
           Reset your password - Step 2/2
           
           Here is your temporary password
           
           >> ${newPassword} <<
         `,
-        html: utils.formatEmail(
-          `<h1> Reset your password - Step 2/2 </h1>
+      html: utils.formatEmail(
+        `<h1> Reset your password - Step 2/2 </h1>
           <h2> Here is your temporary password  </h2>`,
-          `<div style="
+        `<div style="
             padding: 30px;
             border-radius: 10px;
             margin-top: 15px;
@@ -471,48 +495,52 @@ app.v2
           ">
             ${newPassword}
           </div>`
-        ),
+      ),
+    });
+
+    res.sendStatus(200);
+  })
+);
+
+route<types.api.ConfirmEmail>(
+  "Post",
+  "/confirm-email",
+  utils.authentication(),
+  expressAsyncHandler(async (req, res) => {
+    if (!utils.hasAccount(req)) return;
+
+    await utils.sendAccountConfirmation(req.account);
+
+    res.sendStatus(200);
+  })
+);
+
+route<types.api.ConfirmEmail>(
+  "Patch",
+  "/confirm-email",
+  utils.authentication(),
+  expressAsyncHandler(async (req, res) => {
+    if (!utils.hasAccount(req)) return;
+
+    const code: types.api.ConfirmEmail["Methods"]["Patch"]["Body"]["code"] =
+      req.body.code;
+
+    const confirmation = await auth
+      .confirmations()
+      .where("code", code)
+      .and.where("account_id", req.account.id)
+      .first();
+
+    if (!confirmation)
+      return utils.sendError(res, {
+        code: 422,
+        description: "Confirmation code not valid",
       });
 
-      res.sendStatus(200);
-    })
-  );
+    await auth.confirmations().where("account_id", req.account.id).delete();
 
-app.v2
-  .route("/confirm-email")
-  .all(utils.authentication())
-  .post(
-    expressAsyncHandler(async (req, res) => {
-      if (!utils.hasAccount(req)) return;
+    await auth.updateAccount(req.account.id, { confirmed: true });
 
-      await utils.sendAccountConfirmation(req.account);
-
-      res.sendStatus(200);
-    })
-  )
-  .patch(
-    expressAsyncHandler(async (req, res) => {
-      if (!utils.hasAccount(req)) return;
-
-      const code: types.api.ConfirmEmail["Patch"]["Body"]["code"] =
-        req.body.code;
-
-      const confirmation = await auth
-        .confirmations()
-        .where("code", code)
-        .and.where("account_id", req.account.id)
-        .first();
-
-      if (!confirmation)
-        return utils.sendError(res, {
-          code: 422,
-          description: "Confirmation code not valid",
-        });
-
-      await auth.confirmations().where("account_id", req.account.id).delete();
-
-      await auth.updateAccount(req.account.id, { confirmed: true });
-
-      res.sendStatus(200);
-    })
-  );
+    res.sendStatus(200);
+  })
+);
