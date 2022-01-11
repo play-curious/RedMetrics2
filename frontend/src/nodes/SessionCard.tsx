b83e740e0f41e9cef32846938dd8a66edc1bdea3
Jesse Himmelstein <jesse@heavenstone.net>
2022-01-11 17:46:48 +0100

    WIP: fixing typings of SessionCard

--- frontend/src/nodes/SessionCard.tsx
+++ frontend/src/nodes/SessionCard.tsx
@@ -7,14 +7,14 @@ export default function SessionCard({
   session,
   key,
 }: {
-  session: types.tables.Session;
+  session: types.api.SessionById["Methods"]["Get"]["Response"];
   key?: any;
 }) {
   return (
     <Card
       key={key}
-      title={new Date().setTime(Number(session.created_timestamp)).toString()}
-      url={`/game/${session.game_id}/session/show/${session.id}`}
+      title={new Date().setTime(Number(session.createdTimestamp)).toString()}
+      url={`/game/${session.gameId}/session/show/${session.id}`}
       secondary={session.id}
     />
   );
