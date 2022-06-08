1cdeecb33f6cf3513bd3d8541fe8282956a3ddfa
Jesse Himmelstein <jesse@heavenstone.net>
2022-06-08 18:21:48 +0200

    Switched to API_BASE_URL

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 import * as env from "react-dotenv";
 
 export const API_BASE_URL = `${(
-  env.default?.API_URL ?? "http://localhost:6627"
+  env.default?.API_BASE_URL ?? "http://localhost:6627"
 ).replace(/\/$/, "")}/v2/`;
 export const COOKIE_NAME = "rm2-login-cookie";
